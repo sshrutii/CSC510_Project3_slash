40fb0d7ce29156fa0e00dc23f9e4ca7e817975eb
abhi934 <abhishek.triotech@gmail.com>
2023-10-18 18:06:03 -0400

    Bug Fix and drop down added

--- pyscript-local-runtime/popup.js
+++ pyscript-local-runtime/popup.js
@@ -1,18 +1,19 @@
 async function fetchData() {
-    const website = document.getElementById('website').value;
-    const product = document.getElementById('product').value;
-    const response = await fetch(`http://127.0.0.1:5050/${website}/${product}`);
-    const data = await response.json();
-  
-    const resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = '';
-  
-    data.forEach(item => {
-      const itemDiv = document.createElement('div');
-      itemDiv.innerHTML = `<h3>${item.title}</h3><p>Price: ${item.price}</p><a href="${item.link}" target="_blank">Link</a>`;
-      resultsDiv.appendChild(itemDiv);
-    });
-  }
-  
-  document.getElementById('fetchButton').addEventListener('click', fetchData);
-  
+  const website = document.getElementById('website').value;
+  const product = document.getElementById('product').value;
+  const response = await fetch(`http://127.0.0.1:5050/${website}/${product}`);
+  const data = await response.json();
+
+  const resultsDiv = document.getElementById('results');
+  resultsDiv.innerHTML = '';
+
+  data.forEach(item => {
+    // Remove spaces and add a dot to the price
+    const formattedPrice = item.price.replace(/\s/g, '').replace('$', '$ ');
+    const itemDiv = document.createElement('div');
+    itemDiv.innerHTML = `<h3>${item.title}</h3><p>Price: ${formattedPrice}</p><a href="${item.link}" target="_blank">Link</a>`;
+    resultsDiv.appendChild(itemDiv);
+  });
+}
+
+document.getElementById('fetchButton').addEventListener('click', fetchData);
