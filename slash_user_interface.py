a3b0107ceb432c5155f8dc785c4509d3a403eda1
Rishi2812 <56027963+Rishi2812@users.noreply.github.com>
2023-10-18 23:31:00 -0400

    UI update

--- slash_user_interface.py
+++ slash_user_interface.py
@@ -33,73 +33,101 @@ product = st.text_input('Enter the product item name')
 website = st.selectbox('Select the website',('Costco', 'Walmart', 'Ebay', 'BestBuy', 'Target', 'Amazon', 'All'))
 
 website_dict = {
-        'Amazon':'az',
-        'Walmart':'wm',
-        'Ebay':'eb',
-        'BestBuy':'bb',
-        'Target':'tg',
-        'Costco':'cc',
-        'All':'all'
-        }
-# Pass product and website to method
+    'Amazon': 'az',
+    'Walmart': 'wm',
+    'Ebay': 'eb',
+    'BestBuy': 'bb',
+    'Target': 'tg',
+    'Costco': 'cc',
+    'All': 'all'
+}
+
+# Pass product and website to the method
 if st.button('Search') and product and website:
     results = search_items_API(website_dict[website], product)
 
-    # If result is not found on the selected website, search other websites
-    if results == None:
+    # If the result is not found on the selected website, search other websites
+    if results is None:
         for key, value in website_dict.items():
             if key == website:
                 continue
             results = search_items_API(website_dict[key], product)
-            if results == None:
-                continue
+            if results is not None:
+                break
 
-    
     # Use st.columns based on return values
-    description = []
-    url = []
-    price = []
-    site = []
-    
-    if results != None:
+    if results is not None:
+        items = []
+
         for result in results:
-            if result!={} and result['price']!='':
-                description.append(result['title'])
-                url.append(result['link'])
-                price.append(float(''.join(result['price'].split('$')[-1].strip('$').rstrip('0').split(','))))
-                site.append(result['website'])
-            
-    if len(price):
-        
-        def highlight_row(dataframe):
-            #copy df to new - original data are not changed
-            df = dataframe.copy()
-            minimumPrice = df['Price'].min()
-            #set by condition
-            mask = df['Price'] == minimumPrice
-            df.loc[mask, :] = 'background-color: lightgreen'
-            df.loc[~mask,:] = 'background-color: ""'
-            return df
-        
-        dataframe = pd.DataFrame({'Description': description, 'Price':price, 'Link':url, 'Website':site})
-        st.balloons()
-        st.markdown("<h1 style='text-align: center; color: #1DC5A9;'>RESULT</h1>", unsafe_allow_html=True)
-        st.dataframe(dataframe.style.apply(highlight_row, axis=None))
-        st.markdown("<h1 style='text-align: center; color: #1DC5A9;'>Visit the Website</h1>", unsafe_allow_html=True)
-        min_value = min(price)
-        min_idx = [i for i, x in enumerate(price) if x == min_value]
-        for minimum_i in min_idx:
-            link_button_url = shorten_url(url[minimum_i].split('\\')[-1])
-            st.write("Cheapest Product [link]("+link_button_url+")")
-            #link_button(site[minimum_i], link_button_url)
-        
-    else:
-        st.error('Sorry!, there is no other website with same product')
-        
+            if result != {} and result['price'] != '':
+                items.append(
+                    {
+                        'Title': result['title'],
+                        'Price': float(''.join(result['price'].split('$')[-1].strip('$').rstrip('0').split(','))),
+                        'Website': result['website'],
+                        'Link': shorten_url(result['link'].split('\\')[-1])
+                    }
+                )
+
+        if len(items):
+            st.balloons()
+            st.markdown("<h2 style='text-align: center; color: #1DC5A9;'>Search Results</h2>", unsafe_allow_html=True)
+
+            cheapest_item = None
+
+            for item in items:
+                if cheapest_item is None or item['Price'] < cheapest_item['Price']:
+                    cheapest_item = item
+
+            items.remove(cheapest_item)
+            items.append(cheapest_item)
+
+            df = pd.DataFrame(items)
+            #st.table(df)
+            # Find the indexes of the cheapest and second cheapest items
+            cheapest_price = min(items, key=lambda x: x['Price'])['Price']
+            second_cheapest_price = sorted(set(item['Price'] for item in items))[1]
+
+            cheapest_indexes = [i for i, item in enumerate(items) if item['Price'] == cheapest_price]
+            second_cheapest_indexes = [i for i, item in enumerate(items) if item['Price'] == second_cheapest_price]
+
+            # Highlight the rows with the cheapest and second cheapest items
+            def highlight_cheapest(index):
+                background_color = 'background-color: lightgreen' if index in cheapest_indexes or index in second_cheapest_indexes else ''
+                return background_color
+
+            # Create an HTML table with custom styling
+            styled_table = '<table>'
+            for i, item in enumerate(items):
+                style = highlight_cheapest(i)
+                row = f'<tr style="{style}">'
+                row += f'<td>{item["Title"]}</td>'
+                row += f'<td>{item["Price"]}</td>'
+                row += f'<td>{item["Website"]}</td>'
+                row += f'<td><a href="{item["Link"]}">Link to the product</a></td>'
+                row += '</tr>'
+                styled_table += row
+            styled_table += '</table>'
+
+            # Display the HTML table using st.markdown
+            st.markdown(styled_table, unsafe_allow_html=True)
+
+
+
+            st.markdown("<h2 style='text-align: center; color: #1DC5A9;'>Cheapest Item</h2>", unsafe_allow_html=True)
+            st.write("Title:", cheapest_item['Title'])
+            st.write("Price:", cheapest_item['Price'])
+            st.write("Website:", cheapest_item['Website'])
+            st.write("Link:", cheapest_item['Link'])
+
+
+        else:
+            st.error('No results found for the selected product and website.')
 
 
 # Add footer to UI
-footer="""<style>
+footer = """<style>
 a:link , a:visited{
 color: blue;
 background-color: transparent;
@@ -128,5 +156,4 @@ text-align: center;
 <p>Contributors: Anshul, Bhavya, Darshan, Pragna, Rohan</p>
 </div>
 """
-st.markdown(footer,unsafe_allow_html=True)
-
+st.markdown(footer, unsafe_allow_html=True)
